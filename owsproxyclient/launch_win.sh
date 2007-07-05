9a6ce2fac42d69b2a3ebf0fc86fe55a68903705f
sypasche <sypasche@d2b8d2de-b12a-0410-86d9-8c904ad3c895>
2007-07-05 15:51:39 +0000

    fixed paths to use the in tree netbeans project

--- owsproxyclient/launch_win.sh
+++ owsproxyclient/launch_win.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -classpath 'bin;lib/jetty-6.0.1.jar;lib/jetty-util-6.0.1.jar;lib/servlet-api-2.5-6.0.1.jar;lib/commons-httpclient-3.0.1.jar;lib/commons-logging-1.1.jar;lib/commons-logging-adapters-1.1.jar;lib/commons-logging-api-1.1.jar;lib/commons-codec-1.3.jar;/home/sypasche/c2c/projects/wms/netbeans/owsproxyclient/dist/owsproxyclient.jar;/home/sypasche/c2c/projects/wms/netbeans/owsproxyclient/dist/lib/swing-layout-1.0.jar;e:\c2c\projects\wms\netbeans\owsproxyclient\dist\owsproxyclient.jar' com.camptocamp.owsproxy.OWSClient
+java -classpath 'bin;lib/jetty-6.0.1.jar;lib/jetty-util-6.0.1.jar;lib/servlet-api-2.5-6.0.1.jar;lib/commons-httpclient-3.0.1.jar;lib/commons-logging-1.1.jar;lib/commons-logging-adapters-1.1.jar;lib/commons-logging-api-1.1.jar;lib/commons-codec-1.3.jar;netbeans/owsproxyclient/dist/owsproxyclient.jar;netbeans/owsproxyclient/dist/lib/swing-layout-1.0.jar;netbeans\owsproxyclient\dist\owsproxyclient.jar' com.camptocamp.owsproxy.OWSClient
 
