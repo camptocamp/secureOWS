364771670c8727eff6e0d82d44707f7122b35592
sypasche <sypasche@d2b8d2de-b12a-0410-86d9-8c904ad3c895>
2007-07-05 15:44:45 +0000

    added netbeans project for the GUI

---
+++ owsproxyclient/netbeans/owsproxyclient/src/owsproxyclient/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Main.java
+ *
+ * Created on November 1, 2006, 3:52 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package owsproxyclient;
+import java.util.Locale;
+
+/**
+ *
+ * @author sypasche
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.FRENCH);
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new OWSClientGUI().setVisible(true);
+            }
+        });
+        
+    }
+    
+}
