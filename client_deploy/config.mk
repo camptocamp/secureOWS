44fc7a116c46d8d41aa5efa0360d4d40d1d01fbd
sypasche <sypasche@d2b8d2de-b12a-0410-86d9-8c904ad3c895>
2008-03-28 12:53:27 +0000

    added client deploy scripts

---
+++ client_deploy/config.mk
@@ -0,0 +1,12 @@
+# define here the path of your Eclipse workspace
+ECLIPSE_HOME := /path/to/workspace
+
+# define here the path to the keystore used for signing the Webstart application
+KEYSTORE := /path/to/keystore
+
+# Place here the password of your keystore
+KEYSTORE_PASSPHRASE := myp455w0rd 
+
+# Set here the version of the WebStart application to generate
+WEBSTART_VERSION = 0.4
+
